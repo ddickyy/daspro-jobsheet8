a03679b1b64506c53a85f8f36327170978f38e21
ddickyy <dikiidarmawan05x2@gmail.com>
2024-10-30 09:04:30 +0700

    Update RataNilai08.java

--- RataNilai08.java
+++ RataNilai08.java
@@ -4,7 +4,7 @@ public class RataNilai08 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner (System.in);
-        int totalNilai, j = 1, nilaiMhs = 0, rataNilai;
+        float totalNilai, j = 1, nilaiMhs = 0, rataNilai;
 
         int i = 1;
         while (i<=5) {

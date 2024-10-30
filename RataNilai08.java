69194fc7e0a3bcc9711eba2a6560f3e877e6f507
ddickyy <dikiidarmawam05x2@gmail.com>
2024-10-30 07:33:49 +0700

    Selesai

---
+++ RataNilai08.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class RataNilai08 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int totalNilai, j = 1, nilaiMhs = 0, rataNilai;
+
+        int i = 1;
+        while (i<=5) {
+            totalNilai=0;
+            System.out.println("Nilai Mahasiswa ke-" +i);
+        for (j=1;j<=5;j++) {
+            System.out.print("Nilai ke-" +j + " = ");
+            nilaiMhs = sc.nextInt();
+            totalNilai += nilaiMhs;
+        }
+        rataNilai = totalNilai/5;
+        System.out.println("Rata-rata Nilai Mahasiswa ke " +i +" adalah " +rataNilai);
+        i++;
+    }
+}
+}
